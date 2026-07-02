92f81a132d4d8b72a10b3fe68ab88958692150fb
Rodrigo Gerometta <RodriGerometta@users.noreply.github.com>
2026-07-02 18:16:12 -0300

    POO

---
+++ extra/12_POO/hat.py
@@ -0,0 +1,10 @@
+import random
+
+class Hat:
+    houses = ["Gryffindor", "Hufflepuff", "Revenclaw", "Slytherin"]
+    
+    @classmethod
+    def sort(cls, name):
+        print(name, "is in", random.choice(cls.houses))
+    
+Hat.sort("Harry")
--- extra/12_POO/student.py
+++ extra/12_POO/student.py
@@ -1,22 +1,60 @@
 class Student:
-    def __init__(self, name, house):
-        if not name:
-            raise ValueError("Missing name")
-        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
-            raise ValueError("Invalid house")
+    def __init__(self, name, house):           
         self.name = name
         self.house = house
+        # self.patronus = patronus
+
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+    
+    @classmethod
+    def get(cls):
+        name = input("Name: ")
+        house = input("House: ")
+        return cls(name, house)
+    
+    #GET
+    @property
+    def name(self):
+        return self._name
+
+    #SET
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError("Missing name")
+        self._name = name
+    
+    #GET 
+    @property
+    def house(self):
+        return self._house
+    
+    #SET
+    @house.setter
+    def house(self, house):
+        if house not in ["Griffindor", "Hufflepuff", "Revenclaw", "Slytherin"]:
+            raise ValueError("Invalid House")
+        self._house = house
+    
+
+    # def charm(self):
+    #     match self.patronus:
+    #         case "Stag":
+    #             return "🦌"
+    #         case "Otter":
+    #             return "🦦"
+    #         case "Jack Russell terrier":
+    #             return "🐶"
+    #         case _:
+    #             return "🪄"
 
 
 def main():
-    student = get_student()
-    print(f"{student.name} from {student.house}")
-
-
-def get_student():
-    name = input("Name: ")
-    house = input("House: ")
-    return Student(name, house) 
+    student = Student.get()
+    print(student)
+    # print ("Expecto Patronum!")
+    # print(student.charm())
 
 
 if __name__ == "__main__":

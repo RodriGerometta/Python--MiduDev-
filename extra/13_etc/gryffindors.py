1609f2bc63ed7fbfe5403a096b61eddb23909761
Rodrigo Gerometta <RodriGerometta@users.noreply.github.com>
2026-07-06 00:00:23 -0300

    06/07/2026

---
+++ extra/13_etc/gryffindors.py
@@ -0,0 +1,37 @@
+# students = [
+#     {"name": "Hermione", "house": "Gryffindor"},
+#     {"name": "Harry", "house": "Gryffindor"},
+#     {"name": "Ron", "house": "Gryffindor"},
+#     {"name": "Draco", "house": "Slytherin"},
+# ]
+
+# gryffindors = [
+#     student["name"] for student in students if student["house"] == "Gryffindor"
+# ]
+
+# for gryffindor in sorted(gryffindors):
+#     print(gryffindor)
+
+
+# def is_gryffindor(s):
+#     return s["house"] == "Gryffindor"
+
+
+# gryffindors = filter(is_gryffindor, students)
+
+# for gryffindor in sorted(gryffindors, key=lambda s: s["name"]):
+#     print(gryffindor["name"])
+
+
+students = ["Hermione", "Harry", "Ron"]
+
+# gryffindors = []
+
+# for student in students:
+#     gryffindors.append({"name": student, "house": "Gryffindor"})
+    
+# gryffindors = [{"name": student, "house": "Gryffindor"} for student in students]
+
+gryffindors = {student: "Gryffindor" for student in students}
+
+print(gryffindors)
---
+++ extra/13_etc/sleep.py
@@ -0,0 +1,12 @@
+def main():
+    n = int(input("What is n?"))
+    for s in sheep(n):
+        print(s)
+        
+        
+def sheep(n):
+    for i in range(n):
+        yield "🐑" * i
+
+if __name__ == "__main__":
+    main()
